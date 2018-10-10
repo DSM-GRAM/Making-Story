8ea72a0e6f29d9d7108edfa6e701a0d303a894de
donkyey <machun2002@naver.com>
2018-10-10 20:00:56 +0900

    chage img

--- MakingStoryV3/MakingStoryV3/WritingViewController.swift
+++ MakingStoryV3/MakingStoryV3/WritingViewController.swift
@@ -12,6 +12,7 @@ class WritingViewController: UIViewController {
 
     @IBOutlet weak var txtGivenWord: UILabel!
     @IBOutlet weak var txtRegister: UITextView!
+    @IBOutlet weak var btnWordPick: UIButton!
     
     var toGiveWord = ["사람","등","그러나","사회","많다","좋다","받다","그것","집","문제","그녀","자신","문화","생각","소리","여자","개","뒤","함께","아이","시간","인간","사실","어머니","눈","점","시대","다음","누구","여러","하나","세계","위","운동","퍼센트","학교","자기","가장","대통령","가지","바로","그래서","무엇","정부","돈","국가","모두","여성","친구","마음","남성","어른","사랑","학교","남자","창문","하늘","비","크기","고기","서양","냄새","공연","남녀","속도","준비","소년","소식","작용","허리","공업","노인","새","영어","출신","결정","경향","기록","움직임","이미지","특성","교장","업무","입시","청소년","사장","선배","업체","국회","걱정","행사","누나","근처"]
     
@@ -19,6 +20,10 @@ class WritingViewController: UIViewController {
         super.viewDidLoad()
         toGiveWord.shuffle()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        btnWordPick.setImage(UIImage(named: "wordUnpicked2.png"), for: .normal)
     }
     
     @IBAction func btnUnwind(_ sender: Any) {
@@ -33,15 +38,20 @@ class WritingViewController: UIViewController {
         
         let firstAction = UIAlertAction(title: "\(toGiveWord[0])", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
             self.txtGivenWord.text = self.toGiveWord[0]
-            
+            self.txtGivenWord.textColor = UIColor.white
+            self.btnWordPick.setImage(UIImage(named: "wordPicked2.png"), for: .normal)
         }
         
         let secondAction = UIAlertAction(title: "\(toGiveWord[1])", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
             self.txtGivenWord.text = self.toGiveWord[1]
+            self.txtGivenWord.textColor = UIColor.white
+            self.btnWordPick.setImage(UIImage(named: "wordPicked2.png"), for: .normal)
         }
         
         let thirdAction = UIAlertAction(title: "\(toGiveWord[2])", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
             self.txtGivenWord.text = self.toGiveWord[2]
+            self.txtGivenWord.textColor = UIColor.white
+            self.btnWordPick.setImage(UIImage(named: "wordPicked2.png"), for: .normal)
         }
         
         let cancelButton = UIAlertAction(title: "취소", style: UIAlertAction.Style.cancel, handler: nil)
